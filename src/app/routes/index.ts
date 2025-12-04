4ba880c6bfcd929eb3ebbdf82397cb97b9ca97ed
Emon Morol <ekramulhasane69@gmail.com>
2025-12-04 17:53:16 +0600

    created auth module

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,11 +1,15 @@
 import { Router } from 'express';
 
-
+import { AuthRoutes } from '../modules/auth/auth.route';
 import { UserRoutes } from '../modules/user/user.route';
 
 const router = Router();
 
 const moduleRoutes = [
+  {
+    path: '/auth',
+    route: AuthRoutes,
+  },
   {
     path: '/users',
     route: UserRoutes,
